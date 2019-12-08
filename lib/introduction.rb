71a9220ed7616ca6ad45a588fd68056495d61390
calm-block-7153 <rebeccahunt034@gmail.com>
2019-12-08 20:57:04 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -16,8 +16,8 @@ end
 
 introduction_with_language("Tom", "Ruby")
 
-def introduction_with_language_optional (name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language=ruby}"
+def introduction_with_language_optional (name, language=ruby)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 
 introduction_with_language_optional ("Sam")
